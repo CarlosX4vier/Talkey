2f1835d39e2c43ec2519e83f2f1d5825b6fb3e72
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-06 14:24:12 -0300

    Criação de get dos email

--- routerMessage.js
+++ routerMessage.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const errors = require('./errors.json')
-
+const message = require('./ws/message')
 
 function error(code) {
     return { result: code, error: errors[code] }
 }
 
+router.get("/", async function(req, res) {
+    res.send(await message.getEmail(2))
+})
 
 router.post("/", function(req, res) {
     console.log(req);
     res.send(error(400))
 })
 
-module.exports = router
+module.exports = router;

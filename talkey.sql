1789ace36554c2a2363747e75563e4493227ff32
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2019-11-06 19:48:51 -0300

    Banco de dados

--- talkey.sql
+++ talkey.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0.2
--- http://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:3307
--- Generation Time: 06-Nov-2019 às 23:12
--- Versão do servidor: 10.0.17-MariaDB
--- PHP Version: 5.6.14
+-- Host: 127.0.0.1
+-- Generation Time: 06-Nov-2019 às 23:48
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,19 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `conversas`
---
-
-CREATE TABLE `conversas` (
-  `id_conv` int(11) NOT NULL,
-  `user_conv` int(11) NOT NULL,
-  `texto_conv` varchar(255) NOT NULL,
-  `data_conv` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `email`
 --
 
@@ -44,8 +33,17 @@ CREATE TABLE `email` (
   `id_remetente` int(11) NOT NULL,
   `id_destinatario` int(11) NOT NULL,
   `assunto_email` varchar(50) NOT NULL,
-  `message` longtext NOT NULL
+  `mensagem_email` longtext NOT NULL,
+  `pasta_email` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `email`
+--
+
+INSERT INTO `email` (`id_email`, `id_remetente`, `id_destinatario`, `assunto_email`, `mensagem_email`, `pasta_email`) VALUES
+(12, 5, 2, 'Meu email', 'dmasdasda ad afsad asdf sdafasd fsadfas asdfsa', 0),
+(13, 2, 1, 'Oi alguma coisa', 'oilsad sakdosak oa fkasoksdods', 0);
 
 -- --------------------------------------------------------
 
@@ -66,31 +64,22 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_user`, `nome_user`, `sobrenome_user`, `email_user`, `pass_user`) VALUES
-(31, 'asdasd', 'adad', 'asdad@asda.com', 0),
-(32, 'Guasdi', 'Fedaa', 'falsad@adl.com', 1432),
-(33, 'Mateus', 'Vieria', 'matt@adas.com', 12313),
-(34, 'Uais', 'Siala', 'poutr@asd.com', 12343),
-(35, 'asdasd', 'asdasd', 'asda@asda.com', 0),
-(36, 'asfaa2', 'dasda', 'asdad@asd.com', 12313),
-(37, 'Victor Hugo', 'da Silva', 'victor@mail.com', 1234);
+(1, 'victor', 'n sei', '23@gmail.com', 0),
+(2, 'CARLOS AUGUSTO', 'DE PAIVA', 'carlosaugustoxpaiva@gmail.com', 5678),
+(5, 'ai', 'meu cu', 'carloso34o23432@gmami.com', 0),
+(6, 'dmdmedmqw', 'ijdeidjewqidj', 'fkweokdweo@gmail.com', 46453);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `conversas`
---
-ALTER TABLE `conversas`
-  ADD PRIMARY KEY (`id_conv`);
-
---
 -- Indexes for table `email`
 --
 ALTER TABLE `email`
   ADD PRIMARY KEY (`id_email`),
-  ADD KEY `id_remetente` (`id_remetente`),
-  ADD KEY `id_destinatario` (`id_destinatario`);
+  ADD KEY `id_destinatario` (`id_destinatario`),
+  ADD KEY `id_remetente` (`id_remetente`);
 
 --
 -- Indexes for table `usuarios`
@@ -103,15 +92,17 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT for table `conversas`
+-- AUTO_INCREMENT for table `email`
 --
-ALTER TABLE `conversas`
-  MODIFY `id_conv` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `email`
+  MODIFY `id_email` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- Constraints for dumped tables
 --
@@ -120,8 +111,9 @@ ALTER TABLE `usuarios`
 -- Limitadores para a tabela `email`
 --
 ALTER TABLE `email`
-  ADD CONSTRAINT `email_ibfk_1` FOREIGN KEY (`id_remetente`) REFERENCES `usuarios` (`id_user`),
-  ADD CONSTRAINT `email_ibfk_2` FOREIGN KEY (`id_destinatario`) REFERENCES `usuarios` (`id_user`);
+  ADD CONSTRAINT `email_ibfk_1` FOREIGN KEY (`id_destinatario`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `email_ibfk_2` FOREIGN KEY (`id_remetente`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
